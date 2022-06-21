958478c882a47bace3e118449d97fe0efe0756cb
lscalise17 <97422956+lscalise17@users.noreply.github.com>
2022-06-20 20:50:40 -0500

    added synonym.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,12 @@ export default function Dictionary() {
   return (
     <div className="dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+        <input
+          className="box"
+          type="search"
+          autoFocus={true}
+          onChange={handleKeywordChange}
+        />
         <input
           type="submit"
           value="search"
